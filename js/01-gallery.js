5c1b47842305270e4efc313ef1ca3687a8baeeb5
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-09 23:58:12 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const imgMurkup = createImgGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMurkup);
+
+galleryContainer.addEventListener('click', onGalleryImgClick);
+
+function createImgGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join("");
+}
+
+function onGalleryImgClick(evt) {
+    const isImgSwatchEl = evt.target.classList.contains('gallery__image');
+    if (!isImgSwatchEl) {
+        return;
+    }
+    console.log(evt.target);
+}
