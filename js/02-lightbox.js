2a09ac0b049efedc260a7e701f4e4b896d2ec75c
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2022-08-10 18:53:57 +0200

    added linksjs and css simple-lightbox framework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imgMurkup = createImgGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMurkup);
+
+galleryContainer.addEventListener('click', onGalleryImgClick);
+
+function createImgGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+        `;
+    }).join("");
+}
+
+
+function onGalleryImgClick(evt) {
+    evt.preventDefault();
+    const isImgSwatchEl = evt.target.classList.contains('gallery__image');
+    if (!isImgSwatchEl) {
+        return;
+    }
+    console.log(evt.target);
+}
